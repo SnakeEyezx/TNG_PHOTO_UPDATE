c53b8d96f574d972834c3486c430f3f9afc5fd56
SnakeEyezx <88919866+SnakeEyezx@users.noreply.github.com>
2021-09-23 12:02:41 +0300

    some fixes

--- photo2database.py
+++ photo2database.py
@@ -1,4 +1,3 @@
-import base64
 import os
 import datetime
 import pyodbc
@@ -42,16 +41,15 @@ def sync_list_builder():
                         select_query_string = f'SELECT PHOTO_VERSION FROM SIGUR_PHOTO_SYNC WHERE PHOTO_ID = {photo_id}'
                         cursor.execute(select_query_string)
                         result = cursor.fetchone()
-                        # Если запрос пустой, добавляем фото в в базу в бинарном виде(BLOB)
+                        # Если запрос пустой, добавляем фото в базу в бинарном виде(BLOB)
                         if result is None:
-                            insert_query_unit = int(photo_id)
                             q = open(photo_path, 'rb')
                             photo_bin = q.read()
                             q.close()
                             try:
                                 # Определяем формат запроса
                                 tng_insert_query = f"INSERT INTO SIGUR_PHOTO_SYNC(PHOTO_ID, PHOTO_BIN, PHOTO_VERSION) VALUES (?, ?, ?);"
-                                cursor.execute(tng_insert_query, insert_query_unit, photo_bin, photo_mod_date)
+                                cursor.execute(tng_insert_query, int(photo_id), photo_bin, photo_mod_date)
                                 connection.commit()
                             except pyodbc.Error as error:
                                 connection.rollback()
@@ -61,13 +59,12 @@ def sync_list_builder():
                             pass
                         # Если дата позже чем в базе, - обновляем
                         else:
-                            update_query_unit = int(photo_id)
                             q = open(photo_path, 'rb')
                             up_photo_bin = q.read()
                             q.close()
                             try:
                                 tng_update_query = f"UPDATE SIGUR_PHOTO_SYNC SET PHOTO_BIN = ?, PHOTO_VERSION = ? WHERE PHOTO_ID = ?;"
-                                cursor.execute(tng_update_query, up_photo_bin, photo_mod_date, update_query_unit)
+                                cursor.execute(tng_update_query, int(photo_id), photo_mod_date, update_query_unit)
                                 connection.commit()
                             except pyodbc.Error as error:
                                 connection.rollback()
